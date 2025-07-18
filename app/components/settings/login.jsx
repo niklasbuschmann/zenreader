0b6fce9ede6f7273f60de3dd64ab1b4daf7b78a6
Niklas Buschmann <niklasbuschmann@posteo.de>
2025-07-18 22:47:43 +0200

    update solid

--- app/components/settings/login.jsx
+++ app/components/settings/login.jsx
@@ -1,6 +1,6 @@
 import api from '../../api.jsx';
 
-const background = color => ({'border-radius': '2px', padding: '1em 1.5em', margin: '1.5em 0', background: color});
+const background = color => ({'border-radius': '2px', padding: '1em 1.5em', 'margin-bottom': '15em', background: color});
 
 const message = error => {
   if (!error)
@@ -15,7 +15,7 @@ const message = error => {
 const Login = props =>
   <main className="grow">
     <h2>Login</h2>
-    <div style={background((!props.error || props.error.status === 401) ? '#46f' : '#f35')}>{message(props.error)}</div>
+    <div style={background((!props.error || props.error.status === 401) ? '#0c4' : '#f35')}>{message(props.error)}</div>
     <Show when={props.error}>
       <div><input id="email" type="text" placeholder="Username" defaultValue={api.user.name} /></div>
       <div><input id="password" type="password" placeholder="Password" defaultValue={api.user.password} /></div>
