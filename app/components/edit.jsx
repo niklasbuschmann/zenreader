96aaa96aa48d88137c5e236ab675a7b9fa314a40
Niklas Buschmann <niklasbuschmann@posteo.de>
2025-07-19 00:46:39 +0200

    fix

--- app/components/edit.jsx
+++ app/components/edit.jsx
@@ -7,7 +7,7 @@ const values = () => [{
 const Edit = props =>
   <dialog open onClick={() => props.replace([])}>
     <div className="dark edit" onClick={event => event.stopPropagation()}>
-      <main className="grow">
+      <main className="column grow">
         <h2>{props.old.title || 'Subscribe'}</h2>
         <div><input id="title" placeholder="Title" type="text" value={props.old.title || ''} /></div>
         <div><input id="url" placeholder="Link" type="url" value={props.old.url || ''} /></div>
