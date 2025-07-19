e735cbee64fe3b4897ff289ac80981fc8b9337d2
Niklas Buschmann <niklasbuschmann@posteo.de>
2025-07-19 13:29:58 +0200

    fixes

--- app/components/settings/about.jsx
+++ app/components/settings/about.jsx
@@ -4,7 +4,7 @@ const About = props =>
   <>
     <h2>Zen Reader</h2>
     <Github background="#4b6fff" color="white" repo="niklasbuschmann/zenreader" />
-    <a href="https://github.com/niklasbuschmann/zenreader" style={{'text-align': 'center', 'margin-top': '2em'}}><img src="zen.svg" style={{width: '14em', transition: '.4s transform'}} onMouseEnter={event => event.target.style.transform = 'rotate(180deg)'} onMouseOut={event => event.target.style.transform = 'rotate(0deg)'} /></a>
+    <a href="https://github.com/niklasbuschmann/zenreader" style={{'text-align': 'center'}}><img src="zen.svg" style={{width: '14em', transition: '.4s transform'}} onMouseEnter={event => event.target.style.transform = 'rotate(180deg)'} onMouseOut={event => event.target.style.transform = 'rotate(0deg)'} /></a>
     <footer>
       <div>
         <span className="fa fa-code" />
