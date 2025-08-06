54f8a0b384974d6018780b192aeb5d40faffb70d
Niklas Buschmann <niklasbuschmann@posteo.de>
2025-08-06 21:42:15 +0200

    cleanup

--- app/components/settings_about.jsx
+++ app/components/settings_about.jsx
@@ -3,13 +3,9 @@ import Github from './github.jsx';
 const About = props =>
   <>
     <Github background="#46f" color="white" repo="niklasbuschmann/zenreader" />
-    <h2 className="grow"><a href="https://github.com/niklasbuschmann/zenreader" target="_blank" style="font-size: 2em; color: #fff">Zen Reader</a></h2>
+    <h2 className="grow"><a href="https://github.com/niklasbuschmann/zenreader" target="_blank" style="font-size: 2em">Zen Reader</a></h2>
     <div>
-      <span>
-        <span className="fa fa-code" />
-        <span>&nbsp; with &nbsp;</span>
-        <strong style="color: #f45">&lt;3</strong>
-      </span>
+      <span><span className="fa fa-code" />&numsp;with&numsp;<b style="color: #f45">&lt;3</b></span>
       <span>Niklas Buschmann</span>
       <span>2015-2025</span>
     </div>
--- public/index.js
+++ public/index.js
@@ -1417,8 +1417,8 @@ const Github = props => (() => {
   return _el$;
 })();
 
-var _tmpl$$9 = /*#__PURE__*/template(`<h2 class=grow><a href=https://github.com/niklasbuschmann/zenreader target=_blank style=font-size:2em;color:#fff>Zen Reader`),
-  _tmpl$2$4 = /*#__PURE__*/template(`<div><span><span class="fa fa-code"></span><span>&nbsp; with &nbsp;</span><strong style=color:#f45>&lt;3</strong></span><span>Niklas Buschmann</span><span>2015-2025`);
+var _tmpl$$9 = /*#__PURE__*/template(`<h2 class=grow><a href=https://github.com/niklasbuschmann/zenreader target=_blank style=font-size:2em>Zen Reader`),
+  _tmpl$2$4 = /*#__PURE__*/template(`<div><span><span class="fa fa-code"></span>&numsp;with&numsp;<b style=color:#f45>&lt;3</b></span><span>Niklas Buschmann</span><span>2015-2025`);
 const About = props => [createComponent(Github, {
   background: "#46f",
   color: "white",
